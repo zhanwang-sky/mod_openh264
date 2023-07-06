ef27248939a0b4d77f1f82b81943bb8c874be028
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2023-07-06 22:07:33 +0800

    h264 to ppm sequence

--- mod_openh264/main.cpp
+++ mod_openh264/main.cpp
@@ -10,11 +10,12 @@
 #include <string>
 #include <fcntl.h>
 #include <unistd.h>
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include "jpeg_helper.hpp"
 #include "mod_openh264.hpp"
-
-using std::cout;
-using std::cerr;
-using std::endl;
+#include "ppm_helper.hpp"
+#include "yuv_helper.hpp"
 
 // Definitions
 
@@ -54,58 +55,91 @@ private:
 
 // Global Variables
 
+DEFINE_string(input, "", "input.h264, mandatory");
+DEFINE_string(output, "", "output.h264, optional");
+DEFINE_string(pic, "", "pic_%03d.ppm, optional");
+
 unsigned char yuv_buf[3840 * 2160 * 3];
+unsigned char rgb_buf[3840 * 2160 * 3];
 unsigned char nalu_buf[64 * UINT16_MAX];
 
 // Global Functions
 
 int main(int argc, char* argv[]) {
-  if (argc != 3) {
-    cerr << "Usage: ./mod_openh264 <input.h264> <output.h264>\n";
+  FLAGS_logtostderr = true;
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  google::InitGoogleLogging(argv[0]);
+
+  if (FLAGS_input.empty()) {
+    LOG(ERROR) << "Usage: ./mod_openh264 <input.h264> [output.h264] [pic_%03d.ppm]";
     exit(EXIT_FAILURE);
   }
 
-  NaluReader nalu_reader(argv[1]);
+  NaluReader nalu_reader(FLAGS_input.c_str());
   if (!nalu_reader) {
-    cerr << "Fail to open \"" << argv[1] << "\" for reading\n";
+    LOG(ERROR) << "Fail to open \"" << FLAGS_input << "\"";
     exit(EXIT_FAILURE);
   }
 
-  std::ofstream nalu_dumper(argv[2], std::ofstream::binary | std::ofstream::trunc);
-  if (!nalu_dumper) {
-    cerr << "Fail to open \"" << argv[2] << "\" for writing\n";
-    exit(EXIT_FAILURE);
+  std::ofstream nalu_dumper;
+  if (!FLAGS_output.empty()) {
+    nalu_dumper.open(FLAGS_output.c_str(), std::ofstream::binary | std::ofstream::trunc);
+    if (!nalu_dumper) {
+      LOG(ERROR) << "Fail to open \"" << FLAGS_output << "\"";
+      exit(EXIT_FAILURE);
+    }
   }
 
   auto h264_context = mod_openh264::switch_h264_init(true, true, 1920, 1080,
                                                      60.f, 16 * 1024 * 1024);
   if (!h264_context) {
-    cerr << "Fail to init mod_openh264\n";
+    LOG(ERROR) << "Fail to init openh264";
     exit(EXIT_FAILURE);
   }
 
-  mod_openh264::switch_image img = {0};
   int nalu_sz, rc;
   int nr_slices = 0;
   int nr_frames = 0;
+  int nr_errors = 0;
+  mod_openh264::switch_image img = {0};
+
   while ((nalu_sz = nalu_reader.read(nalu_buf, sizeof(nalu_buf))) > 0) {
     ++nr_slices;
-    cout << "Slice " << nr_slices << ": " << nalu_sz << endl;
+    LOG(INFO) << "Slice " << nr_slices << ": " << nalu_sz << " bytes";
+
     rc = mod_openh264::switch_h264_decode(h264_context, nalu_buf, nalu_sz, &img);
     if (rc < 0) {
-      cerr << "Decode error, slice=" << nr_slices << ", rc=" << rc << endl;
+      ++nr_errors;
+      LOG(ERROR) << "Decode error, slice " << nr_slices << ": rc=" << rc;
+      continue;
+    } else if (rc > 0) {
+      // more data required
       continue;
     }
-    if (rc == 0) {
-      ++nr_frames;
-      cout << "Frame " << nr_frames << ": " << img.width << "*" << img.height << endl;
+
+    ++nr_frames;
+    LOG(INFO) << "Frame " << nr_frames << ": " << img.width << "*" << img.height;
+
+    if (!FLAGS_pic.empty()) {
+      char filename[256] = {0};
+      snprintf(filename, sizeof(filename), FLAGS_pic.c_str(), nr_frames);
+      yuv_helper::PlanarYUV_to_RGB(yuv_helper::CS_BT709, yuv_helper::FMT_I420,
+                                   img.width, img.height,
+                                   (const int*) img.stride,
+                                   (const unsigned char**) img.planes,
+                                   rgb_buf);
+      ppm_helper::write_ppm(filename, img.width, img.height, rgb_buf);
+    }
+
+    if (nalu_dumper) {
       while ((nalu_sz = mod_openh264::switch_h264_encode(h264_context, &img, nalu_buf, sizeof(nalu_buf))) > 0) {
         nalu_dumper.write((const char*) nalu_buf, nalu_sz);
       }
     }
   }
 
-  cout << nr_slices << " slices, " << nr_frames << " frames\n";
+  LOG(INFO) << nr_slices << " slices, " << nr_frames << " frames, " << nr_errors << " errors";
 
   return 0;
 }
